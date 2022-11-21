6c5bd5882e1d1a75697ff27270312d18a6920b8b
Antoine Perrot <antoine.iverson@gmail.com>
2022-11-22 00:56:45 +0100

    correction find next ph

--- my_module/planning_optimizer/tools/horaires.py
+++ my_module/planning_optimizer/tools/horaires.py
@@ -13,34 +13,17 @@ def find_next_ph(df_hor: pd.DataFrame, curseur_temps: pd.Timestamp) -> int:
     """
     day0 = curseur_temps.weekday()
     found = False
-
+    str_heure_curseur = str(curseur_temps.time())
     while not found:
-        premieres_plages_horaires_valides = df_hor.loc[df_hor['eeh_sfkperiode'] == day0]
-
-        on_same_day = len(premieres_plages_horaires_valides) > 0 and day0 == curseur_temps.weekday()
-
-        if on_same_day:
-            # Si on a trouvé des plages horaires qui tombent le même jour que le curseur, il faut prendre celle qui
-            # contient le timestamp du curseur.
-            # Exemple, le curseur est lundi à 15h, la personne fait 8h-12 et 13h-17h tous les jours ouvrés. Sa prochaine
-            # plage est donc le lundi sur sa deuxième plage horaire de 13h à 17h.
-            # On s'occupera ensuite de tronquer.
-            premieres_plages_horaires_valides = premieres_plages_horaires_valides.loc[
-                (premieres_plages_horaires_valides['eeh_xheurefin'] > str(curseur_temps.time())),
-            ]
-            found = len(premieres_plages_horaires_valides) > 0
-            if not found:
-                day0 = (day0 + 1) % 7
-        elif len(premieres_plages_horaires_valides) > 0:
-            # si on est pas sur le jour de la date de départ du sprint, alors on prend la première plage horaire
-            # dispo.
-            # Exemple, le sprint commence lundi à 18h, sauf que la personne fait 8h-12 et 13h-17h tous les jours. Elle
-            # commence donc le mardi sur sa première plage horaire, de 8h à 12h.
-            found = True
+        if day0 == curseur_temps.weekday():
+            premieres_plages_horaires_valides = df_hor.loc[ (df_hor['eeh_sfkperiode'] == day0)
+                                                           &(str_heure_curseur < df_hor['eeh_xheurefin'] )]
         else:
+            premieres_plages_horaires_valides = df_hor.loc[df_hor['eeh_sfkperiode'] == day0]
+        found = len(premieres_plages_horaires_valides) > 0
+        if not found:
             # il faut essayer le jour suivant de la semaine.
             day0 = (day0 + 1) % 7
-
     first_plage_horaire_index = premieres_plages_horaires_valides.index[0]
     return first_plage_horaire_index
 
