c2abc582ec01c52c0b7b4b4fcd4b8c24a9aae031
Antoine Perrot <antoine.iverson@gmail.com>
2022-11-25 23:12:29 +0100

    t comm

--- module/tests/planning_optimizer/data_mocker.py
+++ module/tests/planning_optimizer/data_mocker.py
@@ -3,6 +3,8 @@ Module contenant les fonctions de mockage de données pour la fonctionnalité Pl
 """
 import pandas as pd
 import numpy as np
+from random import choices
+from module.tests.task_assigner.data_mocker import generate_unique_ids
 
 
 def mock_dict_priorites_projets(df_tsk: pd.DataFrame) -> dict:
@@ -18,3 +20,39 @@ def mock_dict_priorites_projets(df_tsk: pd.DataFrame) -> dict:
     dict_prio = {id_projet: np.random.randint(0, n_prio) for id_projet in liste_projets}
 
     return dict_prio
+
+
+def mock_df_tsk(dict_hor: dict) -> pd.DataFrame:
+    """
+    Prend un dictionnaire de vrais horaires utilisateurs et génère des tâches aléatoire sur des projets aléatoires.
+
+    Exemple de sortie :
+    	evt_dduree	evt_spkevenement	lgl_sfkligneparent	evt_sfkprojet
+    0	0.25	476	24	3668
+    1	0.50	496	3	3211
+    2	2.75	570	1	3778
+    3	3.50	696	10	2630
+    4	1.75	839	1	2630
+    ...	...	...	...	...
+    295	0.25	9768	8	3668
+    296	2.00	9807	8	1617
+    297	2.75	9876	5	3211
+    298	3.00	9957	10	3778
+    299	3.25	9972	30	2630
+    """
+    liste_utl = list(dict_hor.keys())
+    n_utl = len(liste_utl)
+    n_tasks = 25 * n_utl
+    n_projets = n_utl // 2
+    ids_tasks = generate_unique_ids(n_tasks)
+    ids_prj = generate_unique_ids(n_projets)
+    duree_tasks = np.round(np.random.randint(1, 4 * 4, n_tasks) / 4, 2)
+
+    task_utl = choices(liste_utl, k=n_tasks)
+    task_prj = choices(ids_prj, k=n_tasks)
+    out = pd.DataFrame({"evt_dduree": duree_tasks,
+                        "evt_spkevenement": ids_tasks,
+                        "lgl_sfkligneparent": task_utl,
+                        "evt_sfkprojet": task_prj})
+
+    return out
