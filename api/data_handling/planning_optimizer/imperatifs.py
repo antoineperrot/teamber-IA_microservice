611b79fba74155e354fe56b1da12f3ac17e429ea
Antoine Perrot <antoine.iverson@gmail.com>
2022-11-26 00:12:25 +0100

    ajout data splitter imperatifs

---
+++ api/data_handling/planning_optimizer/imperatifs.py
@@ -0,0 +1,22 @@
+"""
+Fonctions pour préparer les données impératifs brut de Wandeed à l'optimisation
+"""
+import pandas as pd
+
+
+def split_imp(df_imp: pd.DataFrame) -> dict:
+    """
+    Sépare le fichier d'entrée des impératifs en un dictionnaire {id_utilsateur:impératif_utilisateur}
+    """
+
+    dict_imp = {}
+
+    # TODO: corriger la clé quand j'aurai la bonne
+    for utl in df_imp['lgl_sfkligneparent'].unique():
+        df_imp_utl = df_imp.loc[df_imp['lgl_sfkligneparent'] == utl]
+        dict_imp[utl] = df_imp_utl
+
+    return dict_imp
+
+
+splitted_imp = split_imp(df_imp)
