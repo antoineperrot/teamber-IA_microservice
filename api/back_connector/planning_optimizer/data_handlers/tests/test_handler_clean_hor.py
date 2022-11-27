1417549fe6fefedd4ed51d92605d03c043728ef0
Antoine Perrot <antoine.iverson@gmail.com>
2022-11-27 16:54:13 +0100

    correction test

--- api/back_connector/planning_optimizer/data_handlers/tests/test_handler_clean_hor.py
+++ api/back_connector/planning_optimizer/data_handlers/tests/test_handler_clean_hor.py
@@ -1,8 +1,9 @@
 import unittest
 import pandas as pd
+from api.back_connector.planning_optimizer.data_handlers.horaires import handler_clean_hor
 
 
-class TestCleanHor(unittest.TestCase):
+class TestHandlerCleanHor(unittest.TestCase):
     def setUp(self):
         self.list_test_input = []
         self.list_expected_result = []
@@ -19,7 +20,7 @@ class TestCleanHor(unittest.TestCase):
         self.list_expected_result.append(
             pd.DataFrame(
                 {
-                    "eeh_sfkperiode": [1, 2],
+                    "eeh_sfkperiode": [0, 1],
                     "eeh_xheuredebut": ["06:30", "22:45"],
                     "eeh_xheurefin": ["10:45", "23:00"],
                 }
@@ -38,16 +39,16 @@ class TestCleanHor(unittest.TestCase):
         self.list_expected_result.append(
             pd.DataFrame(
                 {
-                    "eeh_sfkperiode": [2],
+                    "eeh_sfkperiode": [1],
                     "eeh_xheuredebut": ["06:30"],
                     "eeh_xheurefin": ["10:45"],
                 }
             )
         )
 
-    def test_clean_hor(self):
+    def test_handler_clean_hor(self):
         for input_value, expected_result in zip(
             self.list_test_input, self.list_expected_result
         ):
-            function_output = clean_hor(input_value)
+            function_output = handler_clean_hor(input_value)
             pd.testing.assert_frame_equal(function_output, expected_result)
