fb1366bf98c3d4b570ead703d9eb15d402ee387d
perrotcap <antoine.perrot@capgemini.com>
2023-03-28 19:46:30 +0200

    dev en cours: correction de la requête

--- api/back_connector/task_assigner/requests/tasks.py
+++ api/back_connector/task_assigner/requests/tasks.py
@@ -5,36 +5,36 @@ from api.string_keys import *
 def get_tasks_request(date_start: str, date_end: str):
     """Request builder for tasks in Task Assigner"""
     return {"select": [
-                key_evenement,
-                key_evenement_project,
-                key_duree_evenement,
-                key_competence,
+        key_evenement,
+        key_evenement_project,
+        key_duree_evenement,
+        key_competence,
+    ],
+        "from": key_table_evenements,
+        "where": {
+            "condition": "and",
+            "rules": [
+                {
+                    "label": key_evenement_date_debut,
+                    "field": key_evenement_date_debut,
+                    "operator": "greaterthan",
+                    "type": "date",
+                    "value": f"{date_start}",
+                },
+                {
+                    "label": key_evenement_date_fin,
+                    "field": key_evenement_date_fin,
+                    "operator": "lessthan",
+                    "type": "date",
+                    "value": f"{date_end}",
+                },
+                {
+                    "label": key_competence,
+                    "field": key_competence,
+                    "operator": "isnotnull",
+                    "type": "integer",
+                    "value": "none",
+                },
             ],
-            "from": "lst_vevenement_py",
-            "where": {
-                "condition": "and",
-                "rules": [
-                    {
-                        "label": "evt_xdate_debut",
-                        "field": "evt_xdate_debut",
-                        "operator": "greaterthan",
-                        "type": "date",
-                        "value": f"{date_start}",
-                    },
-                    {
-                        "label": "evt_xdate_fin",
-                        "field": "evt_xdate_fin",
-                        "operator": "lessthan",
-                        "type": "date",
-                        "value": f"{date_end}",
-                    },
-                    {
-                        "label": key_competence,
-                        "field": key_competence,
-                        "operator": "isnotnull",
-                        "type": "integer",
-                        "value": "none",
-                    },
-                ],
-            },
-        }
+        },
+    }
