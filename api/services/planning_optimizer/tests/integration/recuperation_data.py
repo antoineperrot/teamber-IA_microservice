c87201aa41d4448f0e00b6737b1120f3f8ba83cf
perrotcap <antoine.perrot@capgemini.com>
2023-03-28 19:38:47 +0200

    rename test inte

--- api/services/planning_optimizer/tests/integration/recuperation_data.py
+++ api/services/planning_optimizer/tests/integration/recuperation_data.py
@@ -30,20 +30,20 @@ class TestRecuperationDataPlanningOptimizer(unittest.TestCase):
 
         self.assertTrue(success, msg=f"Le test de récupération {test_name} a fail")
 
-    def test_simple(self):
+    def test_inte_simple(self):
         url ="https://development.api.wandeed.com/api/agrp/1"
         headers = {"Authorization": f"{self.access_token}", "Content-Type": "application/json"}
         r = requests.get(url=url, headers=headers)
         self.assertEqual(200, r.status_code)
 
-    def test_request_tasks(self):
+    def test_inte_request_tasks(self):
         """Teste l'exécution de la requête de récupération des tâches auprès du back wandeed pour la fonction PO"""
         self._test_request(request_builder=get_request_tasks, test_name="tâches pour Planning optimizer")
 
-    def test_request_horaires(self):
+    def test_inte_request_horaires(self):
         """Teste l'exécution de la requête de récupération des horaires auprès du back wandeed pour la fonction PO"""
         self._test_request(request_builder=get_request_horaires, test_name="horaires pour Planning optimizer")
 
-    def test_request_imperatifs(self):
+    def test_inte_request_imperatifs(self):
         """Teste l'exécution de la requête de récupération des imperatifs auprès du back wandeed pour la fonction PO"""
         self._test_request(request_builder=get_request_imperatifs, test_name="impératifs pour Planning optimizer")
